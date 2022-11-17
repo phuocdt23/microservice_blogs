c7c12f9d997d4441f78941671a4a3401df0e02d9
Le Thanh Tung <lttung11@cmcglobal.vn>
2022-11-17 14:10:43 +0700

    done: blogs and comments api

--- blogs/index.js
+++ blogs/index.js
@@ -7,10 +7,13 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/posts', (req, res) => {
-
+  res.send(posts).status(200);
 })
 app.post('/posts', (req, res) => {
-
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = { id, title };
+  res.status(201).send(posts[id]);
 })
 
 
---
+++ comments/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+const app = express();
+const comments = {}
+app.use(bodyParser.json());
+
+
+app.get('/comments', (req, res) => {
+  return res.status(201).send(comments);
+})
+//return all comment of an posts
+app.get('/posts/:id/comments', (req, res) => {
+  const { id: postId } = req.params;
+  return res.status(201).send(comments[postId]);
+})
+app.post('/posts/:id/comments', (req, res) => {
+  const { id: postId } = req.params;
+  const { content } = req.body;
+  const commentId = randomBytes(4).toString('hex');
+  
+  comments[postId] = comments[postId] || [];
+  comments[postId].push({
+    commentId, content, postId
+  });
+  return res.status(201).send(comments)
+})
+
+
+
+
+app.listen(4001, () => {
+  console.log('comment is listening on port 4001');
+})
