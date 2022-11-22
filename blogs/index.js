f4924b35adbf7c5389666f71fbb693291f4a2cbe
Le Thanh Tung <lttung11@cmcglobal.vn>
2022-11-22 14:15:52 +0700

    Emitting Events

--- blogs/index.js
+++ blogs/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto')
 const cors = require('cors');
+const axios = require('axios');
 const posts = {};
 const app = express();
 
@@ -11,11 +12,19 @@ app.use(cors())
 app.get('/posts', (req, res) => {
   res.send(posts).status(200);
 })
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = { id, title };
-  console.log('posts', posts)
+  console.log('posts', posts);
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title
+    }
+  })
+
   res.status(201).send(posts[id]);
 })
 
